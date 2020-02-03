bdc05c6924fa4b5be69556dbb89abf654d9dd409
Pedro Gomes <pedrogomes94@gmail.com>
2020-02-03 20:56:11 +0000

    linting

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import { toUtf8Bytes } from '@ethersproject/strings'
-import { arrayify } from '@ethersproject/bytes'
-import { randomBytes as _randomBytes } from "@ethersproject/random";
-import { sha512 as _sha512, computeHmac, SupportedAlgorithm } from "@ethersproject/sha2"
-import { ec as EC } from 'elliptic'
-import aesJs from 'aes-js'
+import { toUtf8Bytes } from '@ethersproject/strings';
+import { arrayify } from '@ethersproject/bytes';
+import { randomBytes as _randomBytes } from '@ethersproject/random';
+import {
+  sha512 as _sha512,
+  computeHmac,
+  SupportedAlgorithm,
+} from '@ethersproject/sha2';
+import { ec as EC } from 'elliptic';
+import aesJs from 'aes-js';
 
 type Encrypted = {
   ciphertext: Buffer;
@@ -12,17 +16,17 @@ type Encrypted = {
   mac: Buffer;
 };
 
-var ec = new EC("secp256k1");
+var ec = new EC('secp256k1');
 
 const EC_GROUP_ORDER = Buffer.from(
-  "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141",
-  "hex"
+  'fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141',
+  'hex'
 );
 const ZERO32 = Buffer.alloc(32, 0);
 
 function assert(condition: boolean, message: string): void {
   if (!condition) {
-    throw new Error(message || "Assertion failed");
+    throw new Error(message || 'Assertion failed');
   }
 }
 
@@ -46,51 +50,54 @@ function equalConstTime(b1: Buffer, b2: Buffer): boolean {
   }
   var res = 0;
   for (var i = 0; i < b1.length; i++) {
-    res |= b1[i] ^ b2[i]; 
+    res |= b1[i] ^ b2[i];
   }
   return res === 0;
 }
 
 function randomBytes(size: number): Buffer {
-  var arr = _randomBytes(size)
+  var arr = _randomBytes(size);
   return Buffer.from(arr);
 }
 
 function sha512(msg: string): Promise<Uint8Array> {
-  return new Promise(async(resolve) => {
-    const bytes = toUtf8Bytes(msg)
-    var hash = _sha512(bytes)
+  return new Promise(async resolve => {
+    const bytes = toUtf8Bytes(msg);
+    var hash = _sha512(bytes);
     resolve(arrayify(hash));
   });
 }
 
-function aesCbcEncrypt(iv: Buffer, key: Buffer,data: Buffer): Promise<Buffer> {
-  return new Promise((resolve) => {
+function aesCbcEncrypt(iv: Buffer, key: Buffer, data: Buffer): Promise<Buffer> {
+  return new Promise(resolve => {
     var aesCbc = new aesJs.ModeOfOperation.cbc(key, iv);
     var encryptedBytes = aesCbc.encrypt(data);
     resolve(Buffer.from(encryptedBytes));
-  })
+  });
 }
 
-function aesCbcDecrypt(iv: Buffer, key: Buffer,data: Buffer): Promise<Buffer> {
-  return new Promise((resolve) => {
+function aesCbcDecrypt(iv: Buffer, key: Buffer, data: Buffer): Promise<Buffer> {
+  return new Promise(resolve => {
     var aesCbc = new aesJs.ModeOfOperation.cbc(key, iv);
     var encryptedBytes = aesCbc.decrypt(data);
     resolve(Buffer.from(encryptedBytes));
-  })
+  });
 }
 
-
-function hmacSha256Sign(key:Buffer, msg: Buffer): Promise<Buffer> {
-  return new Promise(async(resolve) => {
-    const result = computeHmac(SupportedAlgorithm.sha256, key, msg)
+function hmacSha256Sign(key: Buffer, msg: Buffer): Promise<Buffer> {
+  return new Promise(async resolve => {
+    const result = computeHmac(SupportedAlgorithm.sha256, key, msg);
     resolve(Buffer.from(result));
   });
 }
 
-function hmacSha256Verify(key:Buffer, msg:Buffer, sig:Buffer): Promise<boolean> {
-  return new Promise(async(resolve) => {
-    var expectedSig = await hmacSha256Sign(key, msg)
+function hmacSha256Verify(
+  key: Buffer,
+  msg: Buffer,
+  sig: Buffer
+): Promise<boolean> {
+  return new Promise(async resolve => {
+    var expectedSig = await hmacSha256Sign(key, msg);
     resolve(equalConstTime(expectedSig, sig));
   });
 }
@@ -107,141 +114,143 @@ export function generatePrivate() {
     privateKey = randomBytes(32);
   }
   return privateKey;
-};
+}
 
 export function getPublic(privateKey: Buffer) {
   // This function has sync API so we throw an error immediately.
-  assert(privateKey.length === 32, "Bad private key");
-  assert(isValidPrivateKey(privateKey), "Bad private key");
+  assert(privateKey.length === 32, 'Bad private key');
+  assert(isValidPrivateKey(privateKey), 'Bad private key');
   // XXX(Kagami): `elliptic.utils.encode` returns array for every
   // encoding except `hex`.
   return Buffer.from(ec.keyFromPrivate(privateKey).getPublic('hex'));
-};
+}
 
 /**
  * Get compressed version of public key.
  */
 export function getPublicCompressed(privateKey: Buffer) {
-  
-  assert(privateKey.length === 32, "Bad private key");
-  assert(isValidPrivateKey(privateKey), "Bad private key");
+  assert(privateKey.length === 32, 'Bad private key');
+  assert(isValidPrivateKey(privateKey), 'Bad private key');
   // See https://github.com/wanderer/secp256k1-node/issues/46
   let compressed = true;
   return Buffer.from(
     ec.keyFromPrivate(privateKey).getPublic(compressed, 'hex')
   );
-};
+}
 
-export function sign(privateKey:Buffer, msg: Buffer) {
-  return new Promise(async(resolve) => {
-    assert(privateKey.length === 32, "Bad private key");
-    assert(isValidPrivateKey(privateKey), "Bad private key");
-    assert(msg.length > 0, "Message should not be empty");
-    assert(msg.length <= 32, "Message is too long");
+export function sign(privateKey: Buffer, msg: Buffer) {
+  return new Promise(async resolve => {
+    assert(privateKey.length === 32, 'Bad private key');
+    assert(isValidPrivateKey(privateKey), 'Bad private key');
+    assert(msg.length > 0, 'Message should not be empty');
+    assert(msg.length <= 32, 'Message is too long');
     resolve(Buffer.from(ec.sign(msg, privateKey, { canonical: true }).toDER()));
   });
-};
+}
 
 export function verify(publicKey: Buffer, msg: Buffer, sig: Buffer) {
   return new Promise(function(resolve, reject) {
     assert(
       publicKey.length === 65 || publicKey.length === 33,
-      "Bad public key"
+      'Bad public key'
     );
     if (publicKey.length === 65) {
-      assert(publicKey[0] === 4, "Bad public key");
+      assert(publicKey[0] === 4, 'Bad public key');
     }
     if (publicKey.length === 33) {
-      assert(publicKey[0] === 2 || publicKey[0] === 3, "Bad public key");
+      assert(publicKey[0] === 2 || publicKey[0] === 3, 'Bad public key');
     }
-    assert(msg.length > 0, "Message should not be empty");
-    assert(msg.length <= 32, "Message is too long");
+    assert(msg.length > 0, 'Message should not be empty');
+    assert(msg.length <= 32, 'Message is too long');
     if (ec.verify(msg, sig, publicKey)) {
       resolve(null);
     } else {
-      reject(new Error("Bad signature"));
+      reject(new Error('Bad signature'));
     }
   });
-};
+}
 
-export function derive(privateKeyA: Buffer, publicKeyB: Buffer): Promise<Buffer> {
-  return new Promise(async(resolve) => {
-    assert(Buffer.isBuffer(privateKeyA), "Bad private key");
-    assert(Buffer.isBuffer(publicKeyB), "Bad public key");
-    assert(privateKeyA.length === 32, "Bad private key");
-    assert(isValidPrivateKey(privateKeyA), "Bad private key");
+export function derive(
+  privateKeyA: Buffer,
+  publicKeyB: Buffer
+): Promise<Buffer> {
+  return new Promise(async resolve => {
+    assert(Buffer.isBuffer(privateKeyA), 'Bad private key');
+    assert(Buffer.isBuffer(publicKeyB), 'Bad public key');
+    assert(privateKeyA.length === 32, 'Bad private key');
+    assert(isValidPrivateKey(privateKeyA), 'Bad private key');
     assert(
       publicKeyB.length === 65 || publicKeyB.length === 33,
-      "Bad public key"
+      'Bad public key'
     );
     if (publicKeyB.length === 65) {
-      assert(publicKeyB[0] === 4, "Bad public key");
+      assert(publicKeyB[0] === 4, 'Bad public key');
     }
     if (publicKeyB.length === 33) {
-      assert(publicKeyB[0] === 2 || publicKeyB[0] === 3, "Bad public key");
+      assert(publicKeyB[0] === 2 || publicKeyB[0] === 3, 'Bad public key');
     }
     var keyA = ec.keyFromPrivate(privateKeyA);
     var keyB = ec.keyFromPublic(publicKeyB);
     var Px = keyA.derive(keyB.getPublic()); // BN instance
     resolve(Buffer.from(Px.toArray()));
   });
-};
+}
 
 export function encrypt(publicKeyTo: Buffer, msg: Buffer, opts: Encrypted) {
   opts = opts || {};
   // Tmp variables to save context from flat promises;
-  var iv: Buffer
-  var ephemPublicKey: Buffer
-  var ciphertext: Buffer
-  var macKey: Buffer
-  return new Promise(async(resolve) => {
+  var iv: Buffer;
+  var ephemPublicKey: Buffer;
+  var ciphertext: Buffer;
+  var macKey: Buffer;
+  return new Promise(async resolve => {
     var ephemPrivateKey = randomBytes(32);
     // There is a very unlikely possibility that it is not a valid key
     ephemPublicKey = getPublic(ephemPrivateKey);
     resolve(derive(ephemPrivateKey, publicKeyTo));
   })
     .then((Px: any) => sha512(Px))
-    .then((hash) => {
+    .then(hash => {
       iv = opts.iv || randomBytes(16);
       var encryptionKey = hash.slice(0, 32);
       macKey = Buffer.from(hash.slice(32));
       return aesCbcEncrypt(iv, Buffer.from(encryptionKey), msg);
     })
-    .then((data) => {
+    .then(data => {
       ciphertext = data;
       var dataToMac = Buffer.concat([iv, ephemPublicKey, ciphertext]);
       return hmacSha256Sign(macKey, dataToMac);
     })
-    .then((mac) => {
+    .then(mac => {
       return {
         iv: iv,
         ephemPublicKey: ephemPublicKey,
         ciphertext: ciphertext,
-        mac: mac
+        mac: mac,
       };
     });
-};
+}
 
 export function decrypt(privateKey: Buffer, opts: Encrypted) {
   // Tmp variable to save context from flat promises;
-  var encryptionKey: Buffer
+  var encryptionKey: Buffer;
   return derive(privateKey, opts.ephemPublicKey)
-  .then((Px: any) => sha512(Px))
-    .then((hash) => {
+    .then((Px: any) => sha512(Px))
+    .then(hash => {
       encryptionKey = Buffer.from(hash.slice(0, 32));
       var macKey = hash.slice(32);
       var dataToMac = Buffer.concat([
         opts.iv,
         opts.ephemPublicKey,
-        opts.ciphertext
+        opts.ciphertext,
       ]);
       return hmacSha256Verify(Buffer.from(macKey), dataToMac, opts.mac);
     })
-    .then((macGood) => {
-      assert(macGood, "Bad MAC");
+    .then(macGood => {
+      assert(macGood, 'Bad MAC');
       return aesCbcDecrypt(opts.iv, encryptionKey, opts.ciphertext);
     })
-    .then((msg) => {
+    .then(msg => {
       return Buffer.from(new Uint8Array(msg));
     });
-};
+}
