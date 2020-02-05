4047e7f9605096a1698a47d3c34f61844e7c9938
Pedro Gomes <pedrogomes94@gmail.com>
2020-02-05 12:03:26 +0000

    add secp256k1-node bindings

---
+++ src/secp256k1.ts
@@ -0,0 +1,57 @@
+import * as _secp256k1 from 'secp256k1';
+import { randomBytes } from './random';
+import { bufferToArray } from './util';
+
+const secp256k1 = _secp256k1 as any;
+
+export function createPrivateKey(): Buffer {
+  let privateKey = randomBytes(32);
+  while (!verifyPrivateKey(privateKey)) {
+    privateKey = randomBytes(32);
+  }
+
+  return privateKey;
+}
+
+export function verifyPrivateKey(privateKey: Buffer): boolean {
+  return secp256k1.privateKeyVerify(bufferToArray(privateKey));
+}
+
+export function createPublicKey(
+  privateKey: Buffer,
+  compressed: boolean = true
+): Buffer {
+  const result = secp256k1.publicKeyCreate(
+    bufferToArray(privateKey),
+    compressed
+  );
+  return Buffer.from(result);
+}
+
+export function ecdsaSign(msg: Buffer, privateKey: Buffer): Buffer {
+  const result = secp256k1.ecdsaSign(
+    bufferToArray(msg),
+    bufferToArray(privateKey)
+  );
+  return Buffer.from(result);
+}
+
+export function ecdsaVerify(
+  sig: Buffer,
+  msg: Buffer,
+  publicKey: Buffer
+): boolean {
+  return secp256k1.ecdsaVerify(
+    bufferToArray(sig),
+    bufferToArray(msg),
+    bufferToArray(publicKey)
+  );
+}
+
+export function ecdhDerive(publicKey: Buffer, privateKey: Buffer) {
+  const result = secp256k1.ecdh(
+    bufferToArray(publicKey),
+    bufferToArray(privateKey)
+  );
+  return Buffer.from(result);
+}
