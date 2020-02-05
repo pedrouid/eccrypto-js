d62bcb7c7d7a19d1fb173c2fd356eab2607ef676
Pedro Gomes <pedrogomes94@gmail.com>
2020-02-05 13:42:22 +0000

    improve ecies readability

--- src/ecies.ts
+++ src/ecies.ts
@@ -6,33 +6,38 @@ import { hashSharedKey, hmacSha256Sign, hmacSha256Verify } from './sha2';
 import { Encrypted, PreEncryptOpts } from './types';
 import { assert, isValidPrivateKey } from './validators';
 
+export async function generateEncryptionAndMacKey(
+  privateKey: Buffer,
+  publicKey: Buffer
+) {
+  const sharedKey: Buffer = await derive(privateKey, publicKey);
+  const hash: Uint8Array = await hashSharedKey(sharedKey);
+  const encryptionKey: Buffer = Buffer.from(hash.slice(0, 32));
+  const macKey: Buffer = Buffer.from(hash.slice(32));
+  return { encryptionKey, macKey };
+}
+
 export async function encrypt(
   publicKeyTo: Buffer,
   msg: Buffer,
   opts?: PreEncryptOpts
 ) {
   opts = (opts || {}) as PreEncryptOpts;
-  // Tmp variables to save context from flat promises;
-  let iv: Buffer;
-  let ephemPublicKey: Buffer;
-  let ciphertext: Buffer;
-  let macKey: Buffer;
-  var ephemPrivateKey = opts.ephemPrivateKey || randomBytes(32);
+  let ephemPrivateKey = opts.ephemPrivateKey || randomBytes(32);
   // There is a very unlikely possibility that it is not a valid key
   while (!isValidPrivateKey(ephemPrivateKey)) {
     ephemPrivateKey = opts.ephemPrivateKey || randomBytes(32);
   }
-  ephemPublicKey = getPublic(ephemPrivateKey);
-  const sharedKey = await derive(ephemPrivateKey, publicKeyTo);
-  const hash = await hashSharedKey(sharedKey);
+  const ephemPublicKey: Buffer = getPublic(ephemPrivateKey);
+  const { encryptionKey, macKey } = await generateEncryptionAndMacKey(
+    ephemPrivateKey,
+    publicKeyTo
+  );
+  const iv: Buffer = opts?.iv || randomBytes(16);
+  const data: Buffer = await aesCbcEncrypt(iv, encryptionKey, msg);
 
-  iv = opts?.iv || randomBytes(16);
-  let encryptionKey = hash.slice(0, 32);
-  macKey = Buffer.from(hash.slice(32));
-  const data = await aesCbcEncrypt(iv, Buffer.from(encryptionKey), msg);
-
-  ciphertext = data;
-  let dataToMac = Buffer.concat([iv, ephemPublicKey, ciphertext]);
+  const ciphertext = data;
+  const dataToMac = Buffer.concat([iv, ephemPublicKey, ciphertext]);
   const mac = await hmacSha256Sign(macKey, dataToMac);
 
   return {
@@ -44,23 +49,16 @@ export async function encrypt(
 }
 
 export async function decrypt(privateKey: Buffer, opts: Encrypted) {
-  // Tmp variable to save context from flat promises;
-  let encryptionKey: Buffer;
-  const sharedKey = await derive(privateKey, opts.ephemPublicKey);
-  const hash = await hashSharedKey(sharedKey);
-
-  encryptionKey = Buffer.from(hash.slice(0, 32));
-  let macKey = hash.slice(32);
-  let dataToMac = Buffer.concat([
+  const { encryptionKey, macKey } = await generateEncryptionAndMacKey(
+    privateKey,
+    opts.ephemPublicKey
+  );
+  const dataToMac = Buffer.concat([
     opts.iv,
     opts.ephemPublicKey,
     opts.ciphertext,
   ]);
-  const macGood = await hmacSha256Verify(
-    Buffer.from(macKey),
-    dataToMac,
-    opts.mac
-  );
+  const macGood = await hmacSha256Verify(macKey, dataToMac, opts.mac);
 
   assert(macGood, 'Bad MAC');
   const msg = await aesCbcDecrypt(opts.iv, encryptionKey, opts.ciphertext);
