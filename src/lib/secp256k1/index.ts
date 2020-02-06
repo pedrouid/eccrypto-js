9b14fcc3b3c115fc0114dc46b0d0ef918fc8318a
Pedro Gomes <pedrogomes94@gmail.com>
2020-02-06 12:32:57 +0000

    fix signatures

--- src/lib/secp256k1/index.ts
+++ src/lib/secp256k1/index.ts
@@ -7,6 +7,14 @@ import { randomBytes } from '../../random';
 
 const secp256k1: ISecp256k1 = _secp256k1 as any;
 
+function ensureLength(data: Buffer, expectedLength: number) {
+  const diff = data.length - expectedLength;
+  if (diff > 0) {
+    data = data.slice(diff);
+  }
+  return data;
+}
+
 export function createPrivateKey(): Buffer {
   let privateKey = randomBytes(32);
   while (!verifyPrivateKey(privateKey)) {
@@ -29,7 +37,8 @@ export function createPublicKey(
 
 export function ecdsaSign(msg: Buffer, privateKey: Buffer): Buffer {
   const { signature } = secp256k1.sign(msg, privateKey);
-  return signature;
+  const result = secp256k1.signatureExport(signature);
+  return result;
 }
 
 export function ecdsaVerify(
@@ -37,6 +46,9 @@ export function ecdsaVerify(
   msg: Buffer,
   publicKey: Buffer
 ): boolean {
+  if (sig.length > 64) {
+    sig = secp256k1.signatureImport(sig);
+  }
   return secp256k1.verify(msg, sig, publicKey);
 }
 
@@ -46,8 +58,5 @@ export function ecdhDerive(
   compressed?: boolean
 ) {
   let result = secp256k1.ecdhUnsafe(publicKey, privateKey, compressed);
-  if (result.length === 33) {
-    result = result.slice(1);
-  }
-  return result;
+  return ensureLength(result, 32);
 }
