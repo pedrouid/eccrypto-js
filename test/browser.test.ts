b46d2b2ce47c28eed57dbfd3c6f1f6d9aec64853
Pedro Gomes <pedrogomes94@gmail.com>
2020-02-10 19:10:37 +0100

    test AES across environments

--- test/browser.test.ts
+++ test/browser.test.ts
@@ -1,5 +1,5 @@
 import * as eccryptoJS from '../src';
-import { testRandomBytes } from './common';
+import { testRandomBytes, getTestMessageToEncrypt, compare } from './common';
 import { Crypto } from '@peculiar/webcrypto';
 
 declare global {
@@ -12,21 +12,72 @@ declare global {
 window.msCrypto = new Crypto();
 
 describe('Browser', () => {
-  let length: number;
-  let key: Buffer;
-
-  beforeEach(async () => {
-    length = 32;
-    key = testRandomBytes(length);
+  describe('isBrowser', () => {
+    it('should return true', () => {
+      const result = eccryptoJS.isBrowser();
+      expect(result).toBeTruthy();
+    });
   });
 
-  it('should return true for isBrowser check', () => {
-    const result = eccryptoJS.isBrowser();
-    expect(result).toBeTruthy();
-  });
+  describe('AES', () => {
+    let keyLength: number;
+    let key: Buffer;
+    let ivLength: number;
+    let iv: Buffer;
+    let data: Buffer;
 
-  it('should import key from buffer successfully', async () => {
-    const result = await eccryptoJS.browserImportKey(key);
-    expect(result).toBeTruthy();
+    beforeEach(async () => {
+      keyLength = 32;
+      key = testRandomBytes(keyLength);
+      ivLength = 16;
+      iv = testRandomBytes(ivLength);
+      const toEncrypt = await getTestMessageToEncrypt();
+      data = toEncrypt.msg;
+    });
+
+    it('should import key from buffer successfully', async () => {
+      const result = await eccryptoJS.browserImportKey(key);
+      expect(result).toBeTruthy();
+    });
+
+    it('should encrypt successfully', async () => {
+      const ciphertext = await eccryptoJS.browserAesEncrypt(iv, key, data);
+      expect(ciphertext).toBeTruthy();
+    });
+
+    it('should decrypt successfully', async () => {
+      const ciphertext = await eccryptoJS.browserAesEncrypt(iv, key, data);
+      const result = await eccryptoJS.browserAesDecrypt(iv, key, ciphertext);
+      expect(result).toBeTruthy();
+      expect(compare(data, result)).toBeTruthy();
+    });
+
+    it('ciphertext should be decrypted by NodeJS', async () => {
+      const ciphertext = await eccryptoJS.browserAesEncrypt(iv, key, data);
+      const result = await eccryptoJS.nodeAesDecrypt(iv, key, ciphertext);
+      expect(result).toBeTruthy();
+      expect(compare(data, result)).toBeTruthy();
+    });
+
+    it('should decrypt ciphertext from NodeJS', async () => {
+      const ciphertext = await eccryptoJS.nodeAesEncrypt(iv, key, data);
+      const result = await eccryptoJS.browserAesDecrypt(iv, key, ciphertext);
+      expect(result).toBeTruthy();
+      expect(compare(data, result)).toBeTruthy();
+    });
+
+    it('ciphertext should be decrypted by Fallback', async () => {
+      const ciphertext = await eccryptoJS.browserAesEncrypt(iv, key, data);
+      const result = await eccryptoJS.fallbackAesDecrypt(iv, key, ciphertext);
+      expect(result).toBeTruthy();
+      expect(compare(data, result)).toBeTruthy();
+    });
+
+    it('should decrypt ciphertext from Fallback', async () => {
+      const ciphertext = await eccryptoJS.fallbackAesEncrypt(iv, key, data);
+      const result = await eccryptoJS.browserAesDecrypt(iv, key, ciphertext);
+      expect(result).toBeTruthy();
+      expect(compare(data, result)).toBeTruthy();
+    });
   });
 });
