05b1b66fac7395c1f6ecae16598084dd6febd570
Pedro Gomes <pedrogomes94@gmail.com>
2020-02-10 19:14:00 +0100

    test SHA2 across environments

--- test/node.test.ts
+++ test/node.test.ts
@@ -1,5 +1,10 @@
 import * as eccryptoJS from '../src';
-import { testRandomBytes, getTestMessageToEncrypt, compare } from './common';
+import {
+  testRandomBytes,
+  getTestMessageToEncrypt,
+  compare,
+  TEST_MESSAGE,
+} from './common';
 import { Crypto } from '@peculiar/webcrypto';
 
 declare global {
@@ -73,6 +78,57 @@ describe('NodeJS', () => {
       const result = await eccryptoJS.nodeAesDecrypt(iv, key, ciphertext);
       expect(result).toBeTruthy();
       expect(compare(data, result)).toBeTruthy();
+    });
+  });
+
+  describe('SHA2', () => {
+    const SHA256_HASH =
+      '3819ff1b5125e14102ae429929e815d6fada758d4a6886a03b1b1c64aca3a53a';
+
+    const SHA512_HASH =
+      '1ea15b17a445109c6709d54e8d3e3640ad2d8b87a8b020a2d99e2123d24a42eda8b6d3d71419438a7fe8ac3d8b7f1968113544b7ef4289340a5810f05cb2479f';
+
+    describe('SHA256', () => {
+      let expectedLength: number;
+      let expectedOutput: Buffer;
+
+      beforeEach(async () => {
+        expectedLength = 32;
+        expectedOutput = Buffer.from(SHA256_HASH, 'hex');
+      });
+      it('should hash buffer sucessfully', async () => {
+        const input = Buffer.from(TEST_MESSAGE);
+        const output = await eccryptoJS.nodeSha256(input);
+        expect(compare(output, expectedOutput)).toBeTruthy();
+      });
+
+      it('should output with expected length', async () => {
+        const input = Buffer.from(TEST_MESSAGE);
+        const output = await eccryptoJS.nodeSha256(input);
+        expect(output.length === expectedLength).toBeTruthy();
+      });
+    });
+
+    describe('SHA512', () => {
+      let expectedLength: number;
+      let expectedOutput: Buffer;
+
+      beforeEach(async () => {
+        expectedLength = 64;
+        expectedOutput = Buffer.from(SHA512_HASH, 'hex');
+      });
+
+      it('should hash buffer sucessfully', async () => {
+        const input = Buffer.from(TEST_MESSAGE);
+        const output = await eccryptoJS.nodeSha512(input);
+        expect(compare(output, expectedOutput)).toBeTruthy();
+      });
+
+      it('should output with expected length', async () => {
+        const input = Buffer.from(TEST_MESSAGE);
+        const output = await eccryptoJS.nodeSha512(input);
+        expect(output.length === expectedLength).toBeTruthy();
+      });
     });
   });
 });
