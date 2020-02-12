0ad4e0cffed2f249e711235324ba4e6282484879
Pedro Gomes <pedrogomes94@gmail.com>
2020-02-12 17:00:43 +0100

    transpile only

--- ops/postbuild.sh
+++
@@ -1,6 +0,0 @@
-# rename dist files
-cp ./dist/index.umd.production.min.js ./dist/index.js 
-cp ./dist/index.umd.production.min.js.map ./dist/index.js.map
-
-# delete duplicates
-rm -rf ./dist/index.umd.*
