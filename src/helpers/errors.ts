e60bfefc58fb436c1bb51dc1ea6abd942fd7d23f
Pedro Gomes <pedrogomes94@gmail.com>
2020-03-20 11:37:59 +0100

    error constants

---
+++ src/helpers/errors.ts
@@ -0,0 +1,18 @@
+export const ERROR_BAD_MAC = 'Bad MAC';
+export const ERROR_BAD_PRIVATE_KEY = 'Bad private key';
+export const ERROR_BAD_PUBLIC_KEY = 'Bad public key';
+export const ERROR_EMPTY_MESSAGE = 'Message should not be empty';
+export const ERROR_MESSAGE_TOO_LONG = 'Message is too long';
+export const ERROR_PRIVATE_KEY_SIZE = 'Private key must be 32 bytes.';
+export const ERROR_INVALID_PRIVATE_KEY = 'Invalid private key';
+export const ERROR_PUBLIC_KEY_SIZE = 'Public key must be 33 or 65 bytes.';
+export const ERROR_INVALID_PUBLIC_KEY = 'Invalid public key';
+export const ERROR_DERIVATION_PATH = 'Path must start with "m" or "M"';
+export const ERROR_INVALID_INDEX = 'Invalid index';
+export const ERROR_HARDENED_CHILD_KEY = 'Could not derive hardened child key';
+export const ERROR_VERSION_MISMATCH_PRIVATE_AND_PUBLIC =
+  'Version mismatch: does not match private or public';
+export const ERROR_VERSION_MISMATCH_PRIVATE =
+  'Version mismatch: version does not match private';
+export const ERROR_VERSION_MISMATCH_PUBLIC =
+  'Version mismatch: version does not match public';
