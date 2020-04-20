880720a16ed317648dc411e565a8145b31ccc4aa
Pedro Gomes <pedrogomes94@gmail.com>
2020-04-20 18:42:04 +0200

    revert getNodeCrypto

--- src/lib/node.ts
+++ src/lib/node.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 import {
   HMAC_NODE_ALGO,
   AES_NODE_ALGO,
@@ -7,55 +8,50 @@ import {
 } from '../constants';
 import { concatBuffers } from '../helpers';
 
-export function getNodeCrypto(): any {
-  const crypto = require('crypto');
-  return crypto;
-}
-
 export function nodeRandomBytes(length: number): Buffer {
-  return getNodeCrypto().randomBytes(length);
+  return crypto.randomBytes(length);
 }
 
 export function nodeAesEncrypt(iv: Buffer, key: Buffer, data: Buffer): Buffer {
-  const cipher = getNodeCrypto().createCipheriv(AES_NODE_ALGO, key, iv);
+  const cipher = crypto.createCipheriv(AES_NODE_ALGO, key, iv);
   return concatBuffers(cipher.update(data), cipher.final());
 }
 
 export function nodeAesDecrypt(iv: Buffer, key: Buffer, data: Buffer): Buffer {
-  const decipher = getNodeCrypto().createDecipheriv(AES_NODE_ALGO, key, iv);
+  const decipher = crypto.createDecipheriv(AES_NODE_ALGO, key, iv);
   return concatBuffers(decipher.update(data), decipher.final());
 }
 
 export function nodeHmacSha256Sign(key: Buffer, data: Buffer): Buffer {
-  return getNodeCrypto()
+  return crypto
     .createHmac(HMAC_NODE_ALGO, Buffer.from(key))
     .update(data)
     .digest();
 }
 
 export function nodeHmacSha512Sign(key: Buffer, data: Buffer): Buffer {
-  return getNodeCrypto()
+  return crypto
     .createHmac(SHA512_NODE_ALGO, Buffer.from(key))
     .update(data)
     .digest();
 }
 
 export function nodeSha256(data: Buffer): Buffer {
-  return getNodeCrypto()
+  return crypto
     .createHash(SHA256_NODE_ALGO)
     .update(data)
     .digest();
 }
 
 export function nodeSha512(data: Buffer): Buffer {
-  return getNodeCrypto()
+  return crypto
     .createHash(SHA512_NODE_ALGO)
     .update(data)
     .digest();
 }
 
 export function nodeRipemd160(data: Buffer): Buffer {
-  return getNodeCrypto()
+  return crypto
     .createHash(RIPEMD160_NODE_ALGO)
     .update(data)
     .digest();
